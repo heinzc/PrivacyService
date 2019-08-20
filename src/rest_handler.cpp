7e87a24507401875c891a0b018a978e8812508cd
Christopher Heinz <heinz@cs.uni-kl.de>
2019-08-20 15:29:51 +0200

    basic exception handler for bad requests

--- src/rest_handler.cpp
+++ src/rest_handler.cpp
@@ -6,6 +6,8 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
 
+#include <exception>
+
 
 rest_handler::rest_handler()
 {
@@ -47,24 +49,29 @@ void rest_handler::handle_error(pplx::task<void>& t)
 //
 void rest_handler::handle_get(http_request message) {
 
-    auto paths = http::uri::split_path(http::uri::decode(message.relative_uri().path()));
+    try {
+        auto paths = http::uri::split_path(http::uri::decode(message.relative_uri().path()));
 
-    if(std::find(paths.begin(), paths.end(), "aggregation") != paths.end()) {
-        if(input_counter < 3) {
-            message.reply(status_codes::Forbidden, "not enough inputs provided");
-            return;
-        } else {
-            m_pController->getHE_handler()->aggregate(input_counter);
-            message.reply(status_codes::OK, "OK");
+        if(std::find(paths.begin(), paths.end(), "aggregation") != paths.end()) {
+            if(input_counter < 3) {
+                message.reply(status_codes::Forbidden, "not enough inputs provided");
+                return;
+            } else {
+                m_pController->getHE_handler()->aggregate(input_counter);
+                message.reply(status_codes::OK, "OK");
+                return;
+            }
+        } else if(std::find(paths.begin(), paths.end(), "sum") != paths.end()) {
+            int sum = m_pController->getHE_handler()->getSum();
+            message.reply(status_codes::OK, to_string(sum));
             return;
         }
-    } else if(std::find(paths.begin(), paths.end(), "sum") != paths.end()) {
-        int sum = m_pController->getHE_handler()->getSum();
-        message.reply(status_codes::OK, to_string(sum));
-        return;
-    }
 
-    message.reply(status_codes::NotFound,"WAT?!");
+        message.reply(status_codes::NotFound,"WAT?!");
+    }
+    catch(exception& e) {
+        message.reply(status_codes::BadRequest, e.what());
+    }
     return ;
 
 /*     concurrency::streams::fstream::open_istream(U("static/index.html"), std::ios::in).then([=](concurrency::streams::istream is)
@@ -94,41 +101,45 @@ void rest_handler::handle_get(http_request message) {
 // A POST request
 //
 void rest_handler::handle_post(http_request message) {
+    try{
+        auto paths = http::uri::split_path(http::uri::decode(message.relative_uri().path()));
 
-    auto paths = http::uri::split_path(http::uri::decode(message.relative_uri().path()));
-
-    if(std::find(paths.begin(), paths.end(), "produce") != paths.end()) {
-        string stvalue = message.extract_string().get();
-        produce_ctxt(stvalue);
-        message.reply(status_codes::OK,message.to_string());
-        input_counter++;
-    }
-    else if(std::find(paths.begin(), paths.end(), "encrypt") != paths.end()) {
-        string stvalue = message.extract_string().get();
-        
-        message.reply(status_codes::OK,encrypt_ptxt(stvalue));
-    }
-    else if(std::find(paths.begin(), paths.end(), "add") != paths.end()) {
-        string stvalue = message.extract_string().get();
-
-        m_pController->getHE_handler()->add(stvalue);
-        message.reply(status_codes::OK,"läuft!");
-    }
-    else if(std::find(paths.begin(), paths.end(), "aggregate") != paths.end()) {
-        json::object request_json = message.extract_json().get().as_object();
-        json::array values = request_json.at("values").as_array();
-        std::vector<std::string> vec;
-        for(auto it = values.begin(); it != values.end(); ++it) {
-            std::cout << it->as_string() << std::endl;
-            vec.push_back(it->as_string());
+        if(std::find(paths.begin(), paths.end(), "produce") != paths.end()) {
+            string stvalue = message.extract_string().get();
+            produce_ctxt(stvalue);
+            message.reply(status_codes::OK,message.to_string());
+            input_counter++;
         }
-        //string stvalue = message.extract_string().get();
+        else if(std::find(paths.begin(), paths.end(), "encrypt") != paths.end()) {
+            string stvalue = message.extract_string().get();
+            
+            message.reply(status_codes::OK,encrypt_ptxt(stvalue));
+        }
+        else if(std::find(paths.begin(), paths.end(), "add") != paths.end()) {
+            string stvalue = message.extract_string().get();
 
-        std::string result = m_pController->getHE_handler()->aggregate(vec);
-        message.reply(status_codes::OK, m_pController->getHE_handler()->decrypt(result));
+            m_pController->getHE_handler()->add(stvalue);
+            message.reply(status_codes::OK,"läuft!");
+        }
+        else if(std::find(paths.begin(), paths.end(), "aggregate") != paths.end()) {
+            json::object request_json = message.extract_json().get().as_object();
+            json::array values = request_json.at("values").as_array();
+            std::vector<std::string> vec;
+            for(auto it = values.begin(); it != values.end(); ++it) {
+                std::cout << it->as_string() << std::endl;
+                vec.push_back(it->as_string());
+            }
+            //string stvalue = message.extract_string().get();
+
+            std::string result = m_pController->getHE_handler()->aggregate(vec);
+            message.reply(status_codes::OK, m_pController->getHE_handler()->decrypt(result));
+        }
+
+        message.reply(status_codes::NotFound,"WAT?!");
     }
-
-    message.reply(status_codes::NotFound,"WAT?!");
+    catch(exception& e) {
+        message.reply(status_codes::BadRequest, e.what());
+    }
     return ;
 }
 
