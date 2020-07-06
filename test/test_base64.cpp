2af6cbda2a302dd2b50437daefaa022dfd8f7af3
Christopher Heinz <heinz@cs.uni-kl.de>
2020-07-06 11:44:02 +0200

    automatic collection of sourcefiles in cmake

--- test/test_base64.cpp
+++ test/test_base64.cpp
@@ -2,24 +2,28 @@
 
 #include "../include/base64.h"
 
-BOOST_AUTO_TEST_CASE(EncodeTest)
-{
-    const std::string s = "test";
-    BOOST_CHECK_EQUAL(base64_encode(reinterpret_cast<const unsigned char*>(s.c_str()), s.length()), "dGVzdA==");
-}
+BOOST_AUTO_TEST_SUITE(base64_test_suite)
 
-BOOST_AUTO_TEST_CASE(DecodeTest)
-{
-    const std::string s = "dGVzdA==";
-    BOOST_CHECK_EQUAL(base64_decode(s), "test");
-}
+    BOOST_AUTO_TEST_CASE(EncodeTest)
+    {
+        const std::string s = "test";
+        BOOST_CHECK_EQUAL(base64_encode(reinterpret_cast<const unsigned char*>(s.c_str()), s.length()), "dGVzdA==");
+    }
 
-BOOST_AUTO_TEST_CASE(EncodeDecodeTest)
-{
-    const std::string s = "test";
-    
-    std::string encoded = base64_encode(reinterpret_cast<const unsigned char*>(s.c_str()), s.length());
-    std::string decoded = base64_decode(encoded);
+    BOOST_AUTO_TEST_CASE(DecodeTest)
+    {
+        const std::string s = "dGVzdA==";
+        BOOST_CHECK_EQUAL(base64_decode(s), "test");
+    }
 
-    BOOST_CHECK_EQUAL(decoded, s);
-}
+    BOOST_AUTO_TEST_CASE(EncodeDecodeTest)
+    {
+        const std::string s = "test";
+        
+        std::string encoded = base64_encode(reinterpret_cast<const unsigned char*>(s.c_str()), s.length());
+        std::string decoded = base64_decode(encoded);
+
+        BOOST_CHECK_EQUAL(decoded, s);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ test/test_db_access.cpp
@@ -0,0 +1,12 @@
+#include <boost/test/unit_test.hpp>
+
+#include "../include/db_access.h"
+
+BOOST_AUTO_TEST_SUITE(db_access_test_suite)
+
+    BOOST_AUTO_TEST_CASE(DummyTest)
+    {
+        BOOST_CHECK(true);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
